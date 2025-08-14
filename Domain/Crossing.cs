4a019c04fb0c4d7ecf487d1cfa3b9bbbc8b5ff1f
00 <00@DESKTOP-VJASLA9>
2025-08-14 03:12:16 -0500

    added domain modls, using interfaces for crossing

---
+++ Domain/Crossing.cs
@@ -0,0 +1,13 @@
+﻿namespace tracksideApp00.Domain
+{
+    public class Crossing
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+
+        public double Latitude { get; set; }
+
+        public IList<TrainReport> Reports { get; set; } = new List<TrainReport>();
+    }
+}
---
+++ Domain/TrainReport.cs
@@ -0,0 +1,13 @@
+﻿namespace tracksideApp00.Domain
+{
+    public class TrainReport
+    {
+        public int id {  get; set; }
+        public int CrossingId { get; set; }
+        public Crossing Crossing { get; set; }
+        public DateTime ReportedAt { get; set; }
+
+        public DateTime ClearedAt { get; set; }
+
+    }
+}
